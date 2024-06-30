fc3d3100ee1981a1f9349165ce35eaa9b177bef7
Gustavo Mariano Gomes Teixeira <gustavomariano510@gmail.com>
2024-06-30 19:00:17 -0300

    TELA ALBUM E MUDAÇA HOME
    TELA ALBUM E MUDAÇA HOME

--- codigo/assets/js/auth.js
+++ codigo/assets/js/auth.js
@@ -33,25 +33,3 @@ document.getElementById("deslogar").addEventListener("click", ()=>{
 })
 
 window.addEventListener("load", loadHeader)
-
-async function fetchImages(query, containerId) {
-    const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const imagesContainer = document.getElementById(containerId);
-    imagesContainer.innerHTML = '';
-
-    data.results.forEach(image => {
-        const imgElement = document.createElement('div');
-        imgElement.classList.add('image-item');
-        imgElement.innerHTML = `<img src="${image.urls.small}" alt="${image.alt_description}">`;
-        imagesContainer.appendChild(imgElement);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetchImages('sports', 'sports-images');
-    fetchImages('party', 'party-images');
-});
-
